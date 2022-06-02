7f99944dbc29c9b9b06ca8b3b7ac064d6ae86b09
Mohpann <mohpann@gmail.com>
2022-06-02 19:08:38 +0000

    added navbar and iframe elements to Contact.js

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Slide() {
                         <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="/">Schedule</Nav.Link>
-                            <Nav.Link href="/">Contact</Nav.Link>
+                            <Nav.Link href="/Contact">Contact</Nav.Link>
                             <Nav.Link href="/">Gallery</Nav.Link>
                             <Nav.Link href="/">Instructor</Nav.Link>
                         </Nav>
